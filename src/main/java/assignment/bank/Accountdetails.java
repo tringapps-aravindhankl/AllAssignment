16ba34d510b2f721a7ba29252f38a40c91d1fab4
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-03-03 11:11:51 +0530

    Update Accountdetails.java

--- src/main/java/assignment/bank/Accountdetails.java
+++ src/main/java/assignment/bank/Accountdetails.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Accountdetails {
+class Accountdetails {
 
 	String name;
     int cardnumber;
