836c5db8fc455a34b4b4551ed9707f375b96c104
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-03-03 10:56:00 +0530

    Update Graph.java

--- src/main/java/assignment/maths/Graph.java
+++ src/main/java/assignment/maths/Graph.java
@@ -3,32 +3,30 @@ package assignment.maths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+class Graph implements cloneable {
+	    Integer numberx;
+	    Integer numbery;
+	    Logger log=Logger.getLogger("hi");
 
+	    Graph(Integer firstnumber, Integer secondbnumber) {
+	       this.numberx= firstnumber;
+	       this.numbery= secondnumber;
+	    }
 
-public class Graph implements cloneable{
-	Integer NumberX;
-    Integer NumberY;
-    Logger log=Logger.getLogger("hi");
-
-    Graph(Integer FirstNumber, Integer SecondNumber) {
-       this.NumberX= FirstNumber;
-       this.NumberY= SecondNumber;
-    }
-
-    void active() {
-        boolean condition = NumberX.equals(NumberY);
-        log.log(Level.INFO,()->" "+condition);
-    }
-    Graph helo()
-    {
-        try
-        {
-            return (Graph) super.clone();
-        }
-        catch (CloneNotSupportedException e)
-        {
-            log.info("Cloned error");
-            return this;
-        }
-    }
-}
+	    void active() {
+	        boolean condition = Numberx.equals(Numbery);
+	        log.log(Level.INFO,()->" "+condition);
+	    }
+	    Graph helo()
+	    {
+	        try
+	        {
+	            return (Graph) super.clone();
+	        }
+	        catch (CloneNotSupportedException e)
+	        {
+	            log.info("Cloned error");
+	            return this;
+	        }
+	    }
+	}
